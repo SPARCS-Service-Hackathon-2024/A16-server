39c7a9dac2e657b3d704f39b3e16400b0da4ecc8
2paperstar <lee@paperst.ar>
2024-02-11 23:26:07 +0900

    feat: create prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { ApiConfigService } from './api-config/api-config.service';
 import { ApiConfigModule } from './api-config/api-config.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, ApiConfigModule, AuthModule],
+  imports: [UserModule, ApiConfigModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, ApiConfigService],
 })
