e455c89665232285732ad598c58071fc37aa0c14
2paperstar <lee@paperst.ar>
2024-02-11 15:00:59 +0900

    fix: missing HttpModule imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [UserController],
   providers: [UserService],
 })
