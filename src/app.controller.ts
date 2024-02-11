4ebab8bf98fb34ffb60e622d0e5a423a91239253
2paperstar <lee@paperst.ar>
2024-02-11 22:41:49 +0900

    feat: ping API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'ping' })
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.ping();
   }
 }
