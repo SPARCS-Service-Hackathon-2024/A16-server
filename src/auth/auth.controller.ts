e9e16229385c0b85f38ef94d104f165346796090
2paperstar <lee@paperst.ar>
2024-02-11 23:11:21 +0900

    feat: add auth controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('register/email')
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  @Post('register/kakao')
+  getHello2(): string {
+    return 'Hello World!';
+  }
+
+  @Post('login/email')
+  getHello3(): string {
+    return 'Hello World!';
+  }
+
+  @Post('login/kakao')
+  getHello4(): string {
+    return 'Hello World!';
+  }
+}
