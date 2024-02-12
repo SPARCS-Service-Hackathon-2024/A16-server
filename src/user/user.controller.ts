afb41f661bde93c424360d9a4618a32cf526db25
2paperstar <lee@paperst.ar>
2024-02-12 20:29:17 +0900

    fix: user api tag

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { GetUser } from './get-user.decoration';
 import { UserService } from './user.service';
 
+@ApiTags('user')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({ summary: 'user data' })
-  @ApiBearerAuth()
   @Get('me')
   getUser(@GetUser() user: User) {
     return user;
