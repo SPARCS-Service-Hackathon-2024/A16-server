25f7de58e2aa3deb72d20901b46c21aef65eede0
2paperstar <lee@paperst.ar>
2024-02-14 13:31:21 +0900

    fix: make main controller public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOperation } from '@nestjs/swagger';
+import { Public } from './auth/auth.guard';
 
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
