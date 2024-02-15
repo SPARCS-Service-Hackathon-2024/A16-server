e7795f128153965ea46f4183d8c8b24ea340aba7
2paperstar <lee@paperst.ar>
2024-02-15 14:08:42 +0900

    feat: add review comment model

---
+++ prisma/migrations/20240215045731_add_review_comment/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "ReviewComment" (
+    "id" UUID NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" UUID NOT NULL,
+    "reviewId" UUID NOT NULL,
+
+    CONSTRAINT "ReviewComment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ReviewComment" ADD CONSTRAINT "ReviewComment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReviewComment" ADD CONSTRAINT "ReviewComment_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "Review"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
