6ed3c9b724ead7a57285f628ea3c44f90e39a18d
2paperstar <lee@paperst.ar>
2024-02-16 02:04:23 +0900

    fix: nickname check dto ApiProperty

--- src/auth/dto/nickname-check.dto.ts
+++ src/auth/dto/nickname-check.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class NicknameCheckDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   nickname: string;
