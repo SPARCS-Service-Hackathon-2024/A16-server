08e990a953a62081b5e4048f1988bd67fd237518
2paperstar <lee@paperst.ar>
2024-02-16 06:44:30 +0900

    fix: body parser import method

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import bodyParser from 'body-parser';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
