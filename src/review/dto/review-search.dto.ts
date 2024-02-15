304ebb0f96e568861e52641d8d05e467737e6542
2paperstar <lee@paperst.ar>
2024-02-15 14:24:50 +0900

    fix: skip, take as Number

--- src/review/dto/review-search.dto.ts
+++ src/review/dto/review-search.dto.ts
@@ -41,10 +41,12 @@ export class ReviewSearchDto {
   readonly tags: string[] = [];
 
   @ApiProperty({ required: false })
+  @Type(() => Number)
   @Min(0)
   readonly skip: number = 0;
 
   @ApiProperty({ required: false })
+  @Type(() => Number)
   @Max(100)
   readonly take: number = 10;
 }
