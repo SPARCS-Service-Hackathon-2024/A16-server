d1ab03331e0ed432c2c39ea7a61dbf8d84300ed4
2paperstar <lee@paperst.ar>
2024-02-16 06:42:37 +0900

    chore: increase body limit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb' }));
   const config = new DocumentBuilder()
     .setTitle('Wayu API')
     .setDescription('API Documentation for Wayu')
